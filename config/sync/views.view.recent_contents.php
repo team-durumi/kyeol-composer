be571c0f9d8a9c4613cca1e0af8ac629322532e4
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-11-29 10:26:49 +0900

    [카테고리] 영문웹진 (콘텐츠 분류 / static page 메뉴) #10 좌담은 Discussion 입니다.

--- config/sync/views.view.recent_contents.php
+++ config/sync/views.view.recent_contents.php
@@ -14,10 +14,15 @@ $view->disabled = FALSE; /* Edit this to true to make a default view disabled in
 $handler = $view->new_display('default', 'Master', 'default');
 $handler->display->display_options['title'] = 'Recent contents';
 $handler->display->display_options['use_more_always'] = FALSE;
+$handler->display->display_options['use_more_text'] = '더 보기';
 $handler->display->display_options['access']['type'] = 'perm';
 $handler->display->display_options['cache']['type'] = 'none';
 $handler->display->display_options['query']['type'] = 'views_query';
 $handler->display->display_options['exposed_form']['type'] = 'basic';
+$handler->display->display_options['exposed_form']['options']['submit_button'] = '적용';
+$handler->display->display_options['exposed_form']['options']['reset_button_label'] = '재설정';
+$handler->display->display_options['exposed_form']['options']['exposed_sorts_label'] = '정렬기준';
+$handler->display->display_options['exposed_form']['options']['sort_desc_label'] = '설명';
 $handler->display->display_options['pager']['type'] = 'some';
 $handler->display->display_options['pager']['options']['items_per_page'] = '3';
 $handler->display->display_options['pager']['options']['offset'] = '0';
@@ -27,40 +32,40 @@ $handler->display->display_options['style_options']['row_class_special'] = FALSE
 $handler->display->display_options['style_options']['wrapper_class'] = 'lc02 lc02_01';
 $handler->display->display_options['row_plugin'] = 'node';
 $handler->display->display_options['row_options']['view_mode'] = 'teaser_en';
-/* Field: Content: Title */
+/* 필드: 콘텐츠: 제목 */
 $handler->display->display_options['fields']['title']['id'] = 'title';
 $handler->display->display_options['fields']['title']['table'] = 'node';
 $handler->display->display_options['fields']['title']['field'] = 'title';
 $handler->display->display_options['fields']['title']['label'] = '';
 $handler->display->display_options['fields']['title']['alter']['word_boundary'] = FALSE;
 $handler->display->display_options['fields']['title']['alter']['ellipsis'] = FALSE;
-/* Sort criterion: Content: Post date */
+/* 정렬 기준: 콘텐츠: Post date */
 $handler->display->display_options['sorts']['created']['id'] = 'created';
 $handler->display->display_options['sorts']['created']['table'] = 'node';
 $handler->display->display_options['sorts']['created']['field'] = 'created';
 $handler->display->display_options['sorts']['created']['order'] = 'DESC';
-/* Filter criterion: Content: Published status */
+/* 필터 속성: 콘텐츠: Published status */
 $handler->display->display_options['filters']['status']['id'] = 'status';
 $handler->display->display_options['filters']['status']['table'] = 'node';
 $handler->display->display_options['filters']['status']['field'] = 'status';
 $handler->display->display_options['filters']['status']['value'] = 1;
 $handler->display->display_options['filters']['status']['group'] = 1;
 $handler->display->display_options['filters']['status']['expose']['operator'] = FALSE;
-/* Filter criterion: Content: Type */
+/* 필터 속성: 콘텐츠: 종류 */
 $handler->display->display_options['filters']['type']['id'] = 'type';
 $handler->display->display_options['filters']['type']['table'] = 'node';
 $handler->display->display_options['filters']['type']['field'] = 'type';
 $handler->display->display_options['filters']['type']['value'] = array(
   'article' => 'article',
 );
-/* Filter criterion: Content: Title (title_field:language) */
+/* 필터 속성: 콘텐츠: Title (title_field:언어) */
 $handler->display->display_options['filters']['language']['id'] = 'language';
 $handler->display->display_options['filters']['language']['table'] = 'field_data_title_field';
 $handler->display->display_options['filters']['language']['field'] = 'language';
 $handler->display->display_options['filters']['language']['value'] = array(
   '***CURRENT_LANGUAGE***' => '***CURRENT_LANGUAGE***',
 );
-/* Filter criterion: Content: 분류 (field_category) */
+/* 필터 속성: 콘텐츠: 분류 (field_category) */
 $handler->display->display_options['filters']['field_category_tid']['id'] = 'field_category_tid';
 $handler->display->display_options['filters']['field_category_tid']['table'] = 'field_data_field_category';
 $handler->display->display_options['filters']['field_category_tid']['field'] = 'field_category_tid';
@@ -73,28 +78,28 @@ $handler->display->display_options['defaults']['title'] = FALSE;
 $handler->display->display_options['title'] = 'Essay';
 $handler->display->display_options['defaults']['filter_groups'] = FALSE;
 $handler->display->display_options['defaults']['filters'] = FALSE;
-/* Filter criterion: Content: Published status */
+/* 필터 속성: 콘텐츠: Published status */
 $handler->display->display_options['filters']['status']['id'] = 'status';
 $handler->display->display_options['filters']['status']['table'] = 'node';
 $handler->display->display_options['filters']['status']['field'] = 'status';
 $handler->display->display_options['filters']['status']['value'] = 1;
 $handler->display->display_options['filters']['status']['group'] = 1;
 $handler->display->display_options['filters']['status']['expose']['operator'] = FALSE;
-/* Filter criterion: Content: Type */
+/* 필터 속성: 콘텐츠: 종류 */
 $handler->display->display_options['filters']['type']['id'] = 'type';
 $handler->display->display_options['filters']['type']['table'] = 'node';
 $handler->display->display_options['filters']['type']['field'] = 'type';
 $handler->display->display_options['filters']['type']['value'] = array(
   'article' => 'article',
 );
-/* Filter criterion: Content: Title (title_field:language) */
+/* 필터 속성: 콘텐츠: Title (title_field:언어) */
 $handler->display->display_options['filters']['language']['id'] = 'language';
 $handler->display->display_options['filters']['language']['table'] = 'field_data_title_field';
 $handler->display->display_options['filters']['language']['field'] = 'language';
 $handler->display->display_options['filters']['language']['value'] = array(
   '***CURRENT_LANGUAGE***' => '***CURRENT_LANGUAGE***',
 );
-/* Filter criterion: Content: 분류 (field_category) */
+/* 필터 속성: 콘텐츠: 분류 (field_category) */
 $handler->display->display_options['filters']['field_category_tid']['id'] = 'field_category_tid';
 $handler->display->display_options['filters']['field_category_tid']['table'] = 'field_data_field_category';
 $handler->display->display_options['filters']['field_category_tid']['field'] = 'field_category_tid';
@@ -105,8 +110,8 @@ $handler->display->display_options['filters']['field_category_tid']['type'] = 's
 $handler->display->display_options['filters']['field_category_tid']['vocabulary'] = 'article_category';
 $handler->display->display_options['block_description'] = 'Essay';
 
-/* Display: Interview */
-$handler = $view->new_display('block', 'Interview', 'interview');
+/* Display: interview */
+$handler = $view->new_display('block', 'interview', 'interview');
 $handler->display->display_options['defaults']['title'] = FALSE;
 $handler->display->display_options['title'] = 'Interview';
 $handler->display->display_options['defaults']['pager'] = FALSE;
@@ -115,28 +120,28 @@ $handler->display->display_options['pager']['options']['items_per_page'] = '2';
 $handler->display->display_options['pager']['options']['offset'] = '0';
 $handler->display->display_options['defaults']['filter_groups'] = FALSE;
 $handler->display->display_options['defaults']['filters'] = FALSE;
-/* Filter criterion: Content: Published status */
+/* 필터 속성: 콘텐츠: Published status */
 $handler->display->display_options['filters']['status']['id'] = 'status';
 $handler->display->display_options['filters']['status']['table'] = 'node';
 $handler->display->display_options['filters']['status']['field'] = 'status';
 $handler->display->display_options['filters']['status']['value'] = 1;
 $handler->display->display_options['filters']['status']['group'] = 1;
 $handler->display->display_options['filters']['status']['expose']['operator'] = FALSE;
-/* Filter criterion: Content: Type */
+/* 필터 속성: 콘텐츠: 종류 */
 $handler->display->display_options['filters']['type']['id'] = 'type';
 $handler->display->display_options['filters']['type']['table'] = 'node';
 $handler->display->display_options['filters']['type']['field'] = 'type';
 $handler->display->display_options['filters']['type']['value'] = array(
   'article' => 'article',
 );
-/* Filter criterion: Content: Title (title_field:language) */
+/* 필터 속성: 콘텐츠: Title (title_field:언어) */
 $handler->display->display_options['filters']['language']['id'] = 'language';
 $handler->display->display_options['filters']['language']['table'] = 'field_data_title_field';
 $handler->display->display_options['filters']['language']['field'] = 'language';
 $handler->display->display_options['filters']['language']['value'] = array(
   '***CURRENT_LANGUAGE***' => '***CURRENT_LANGUAGE***',
 );
-/* Filter criterion: Content: 분류 (field_category) */
+/* 필터 속성: 콘텐츠: 분류 (field_category) */
 $handler->display->display_options['filters']['field_category_tid']['id'] = 'field_category_tid';
 $handler->display->display_options['filters']['field_category_tid']['table'] = 'field_data_field_category';
 $handler->display->display_options['filters']['field_category_tid']['field'] = 'field_category_tid';
@@ -147,59 +152,61 @@ $handler->display->display_options['filters']['field_category_tid']['type'] = 's
 $handler->display->display_options['filters']['field_category_tid']['vocabulary'] = 'article_category';
 $handler->display->display_options['block_description'] = 'Interview';
 
-/* Display: Researcher Forum */
-$handler = $view->new_display('block', 'Researcher Forum', 'resources');
+/* Display: discussion */
+$handler = $view->new_display('block', 'discussion', 'discussion');
 $handler->display->display_options['defaults']['title'] = FALSE;
-$handler->display->display_options['title'] = 'Researcher Forum';
+$handler->display->display_options['title'] = 'Discussion';
 $handler->display->display_options['defaults']['pager'] = FALSE;
 $handler->display->display_options['pager']['type'] = 'some';
 $handler->display->display_options['pager']['options']['items_per_page'] = '2';
 $handler->display->display_options['pager']['options']['offset'] = '0';
 $handler->display->display_options['defaults']['filter_groups'] = FALSE;
 $handler->display->display_options['defaults']['filters'] = FALSE;
-/* Filter criterion: Content: Published status */
+/* 필터 속성: 콘텐츠: Published status */
 $handler->display->display_options['filters']['status']['id'] = 'status';
 $handler->display->display_options['filters']['status']['table'] = 'node';
 $handler->display->display_options['filters']['status']['field'] = 'status';
 $handler->display->display_options['filters']['status']['value'] = 1;
 $handler->display->display_options['filters']['status']['group'] = 1;
 $handler->display->display_options['filters']['status']['expose']['operator'] = FALSE;
-/* Filter criterion: Content: Type */
+/* 필터 속성: 콘텐츠: 종류 */
 $handler->display->display_options['filters']['type']['id'] = 'type';
 $handler->display->display_options['filters']['type']['table'] = 'node';
 $handler->display->display_options['filters']['type']['field'] = 'type';
 $handler->display->display_options['filters']['type']['value'] = array(
   'data' => 'data',
 );
-/* Filter criterion: Content: Title (title_field:language) */
+/* 필터 속성: 콘텐츠: Title (title_field:언어) */
 $handler->display->display_options['filters']['language']['id'] = 'language';
 $handler->display->display_options['filters']['language']['table'] = 'field_data_title_field';
 $handler->display->display_options['filters']['language']['field'] = 'language';
 $handler->display->display_options['filters']['language']['value'] = array(
   '***CURRENT_LANGUAGE***' => '***CURRENT_LANGUAGE***',
 );
-/* Filter criterion: Content: 분류 (field_category) */
+/* 필터 속성: 콘텐츠: 분류 (field_category) */
 $handler->display->display_options['filters']['field_category_tid']['id'] = 'field_category_tid';
 $handler->display->display_options['filters']['field_category_tid']['table'] = 'field_data_field_category';
 $handler->display->display_options['filters']['field_category_tid']['field'] = 'field_category_tid';
 $handler->display->display_options['filters']['field_category_tid']['operator'] = 'not';
 $handler->display->display_options['filters']['field_category_tid']['value'] = array(
-  7 => '7',
+  2 => '2',
 );
 $handler->display->display_options['filters']['field_category_tid']['type'] = 'select';
 $handler->display->display_options['filters']['field_category_tid']['vocabulary'] = 'article_category';
-$handler->display->display_options['block_description'] = 'Researcher Forum';
+$handler->display->display_options['block_description'] = 'Discussion';
 $translatables['recent_contents'] = array(
   t('Master'),
   t('Recent contents'),
-  t('more'),
-  t('Apply'),
-  t('Reset'),
-  t('Sort by'),
+  t('더 보기'),
+  t('적용'),
+  t('재설정'),
+  t('정렬기준'),
   t('Asc'),
-  t('Desc'),
+  t('설명'),
   t('essay'),
   t('Essay'),
+  t('interview'),
   t('Interview'),
-  t('Researcher Forum'),
+  t('discussion'),
+  t('Discussion'),
 );

bad8f783bdd72f011b2b620408a84c52b5cb49fc
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-11-29 12:19:44 +0900

    홈페이지 3단 소개 블록 read more에 링크를 추가합니다. 메인 (추가 고객 피드백) / Header #13

--- web/sites/all/modules/custom/slowalk/modules/block/templates/about-three-cols.tpl.php
+++ web/sites/all/modules/custom/slowalk/modules/block/templates/about-three-cols.tpl.php
@@ -9,7 +9,7 @@
           </dt>
           <dd>
             <p>"Comfort Women" refer to women who were forcibly mobilized and forced to live as sex slaves in brothels called ‘comfort stations’ established by the Japanese military under the pretext of efficiently carrying out war after September 18, 1931, when the Manchurian Incident took place up until when Japan was defeated in the Pacific War in 1945. In literature and testimonies, ‘comfort women’ were referred to as hostesses, special women, prostitutes, geishas, street girls, waitresses, etc. while ‘comfort stations’ were referred to as army recreation facilities, clubs, servicemen centers or Joseon restaurants.</p>
-            <em>Read more</em>
+            <a href="/en/comfort-women"><em>Read more</em></a>
           </dd>
         </dl>
       </li>
@@ -21,7 +21,7 @@
           </dt>
           <dd>
             <p>Reseach Institute on Japanese Military Sexual Slavery(RIMSS) was established in year 2018 to advance gender equality and women’s human rights. It supports projects aimed at preventing and eradicating violence against women, as well as providing victim-support. Since 2018 the Institute has been keen on conducting systematic research on the issue of Japanese military sexual slavery. </p>
-            <em>Read more</em>
+            <a href="/en/rimss"><em>Read more</em></a>
           </dd>
         </dl>
       </li>
@@ -33,7 +33,7 @@
           </dt>
           <dd>
             <p>We have named this webzine ‘KYOEL’ to signal our intent to delve into and unravel a complex layer of intertwined issues – surrounding the military sexual slavery, such as colonial rule, armed conflict, sexual violence and human rights.</p>
-            <em>Read more</em>
+            <a href="/en/kyeol"><em>Read more</em></a>
           </dd>
         </dl>
       </li>

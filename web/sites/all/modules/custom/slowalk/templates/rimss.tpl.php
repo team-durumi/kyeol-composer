c881b05676cd06a28c7526f6b4c8699d21c5083f
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-12-01 15:49:31 +0900

    소개 페이지에서 캡션을 출력합니다. [영문] '일본군'위안부 소개 페이지 #7

--- web/sites/all/modules/custom/slowalk/templates/rimss.tpl.php
+++ web/sites/all/modules/custom/slowalk/templates/rimss.tpl.php
@@ -5,7 +5,9 @@
         <div class="cBody">
           <!-- con01 -->
           <div class="con01 postA">
-            <div class="banner" style="background-image:url(/sites/all/modules/custom/slowalk/images/banner_introduce.png)"><img src="/sites/all/modules/custom/slowalk/images/banner_introduce.png" alt="일본군 위안부 문제연구소 사무실 입구 모습"/></div>
+            <div class="banner" style="background-image:url(/sites/all/modules/custom/slowalk/images/banner_introduce.png)">
+              <img src="/sites/all/modules/custom/slowalk/images/banner_introduce.png" alt="RIMSS office enterance" />
+            </div>
             <dl class="unit unit01">
               <dt>Research Institute on Japanese Military Sexual Slavery(RIMSS)</dt>
               <br/><br /><br />

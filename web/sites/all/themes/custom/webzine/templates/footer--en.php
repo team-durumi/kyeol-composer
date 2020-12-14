911017c909589cd21f5e297e35b2e6891dae9352
woonjjang <woonjjang@gmail.com>
2020-12-14 18:02:43 +0900

    Update footer--en.php
    푸터텍스트를 수정합니다.

--- web/sites/all/themes/custom/webzine/templates/footer--en.php
+++ web/sites/all/themes/custom/webzine/templates/footer--en.php
@@ -5,7 +5,7 @@
 <ul class="infoA">
   <li class="l1"><strong>Publish :</strong><span>RIMSS</span></li>
   <li class="l2"><strong>Planning :</strong><a href="">Somethingspecial.studio</a></li>
-  <li class="l3"><strong>Design :</strong><a href="">Team Durumi</a></li>
+  <li class="l3"><strong>Design&Dev :</strong><a href="http://durumi.io/">Team Durumi</a></li>
   <li class="l4"><strong>Edit :</strong><span>Sora Kim</span></li>
-  <li class="l5"><strong>Contact :</strong><a href="/cdn-cgi/l/email-protection#a4d6c1c9c1c9c6c1d69c9590e4d7d0cbd48acbd68acfd684"><span class="__cf_email__" data-cfemail="2557404840484740571d14116556514a550b4a570b4e57">[email&#160;protected]</span></a></li>
+  <li class="l5"><strong>Contact :</strong><a href="mailto:remember814@stop.or.kr ">remember814@stop.or.kr</a></li>
 </ul>

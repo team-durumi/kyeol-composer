8600a405c51409321ce45849852fb16deed76c26
yeongtaek, Hong <mozodev@users.noreply.github.com>
2020-12-22 17:52:24 +0900

    [메인] 카테고리 제목 - 최종이라고 하지만 ... #27

--- web/sites/all/themes/custom/webzine/template.php
+++ web/sites/all/themes/custom/webzine/template.php
@@ -152,7 +152,7 @@ function webzine_preprocess_page(&$variables) {
         $variables['page']['content']['body']['box']['recents'][$field] = [
           '#type' => 'markup',
           '#markup' => views_embed_view('recent_contents', $field),
-          '#prefix' => '<h2 class="mt-5" onclick="this.style.display=\'none\'">' . ucwords(str_replace('_' , ' ', $field)) . '</h2>'
+          '#prefix' => ucwords(str_replace('_' , ' ', $field)),
         ];
       }
     }

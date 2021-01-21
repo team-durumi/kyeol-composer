6900d1304b22d0472458b8dc3015c61e24204d5c
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-01-21 19:53:11 +0900

    [모바일] 메뉴 정리 #23 [GNB] 영문 메뉴 #31

--- web/sites/all/themes/custom/webzine/templates/header--en.php
+++ web/sites/all/themes/custom/webzine/templates/header--en.php
@@ -15,14 +15,35 @@
       <li class="mx-6 float-left">
         <a href="/en/category/articles">Contents</a>
       </li>
+      <!--
       <li class="mx-6 float-left">
         <a href="/en/resources">Resource</a>
       </li>
+      -->
     </ul>
     <ul class="ng01">
       <li class="search"><a href="#" title="Open search box"><i class="xi-search"></i></a></li>
-      <li class="translate"><a href="/" title="Translate" ><i style="font-size: 1.955rem">KR</i></a></li>
+      <li class="translate"><a href="/" title="Translate" ><span>KR</span></a></li>
+      <li class="menu"><a href="#" title="메뉴 열기"><i class="xi-bars"></i></a></li>
     </ul>
+    <nav>
+      <a href="#" class="btn_icon01" title="메뉴 닫기"><i class="xi-close"></i></a>
+      <h2>주 메뉴</h2>
+      <ul id="main-menu" class="links inline clearfix">
+        <li class="menu-520 first"><a href="/en/taxonomy/term/7" title="">Interview</a></li>
+        <li class="menu-519"><a href="/en/taxonomy/term/4" title="">Essay</a></li>
+        <li class="menu-518"><a href="/en/taxonomy/term/3" title="">Comment</a></li>
+        <li class="menu-515"><a href="/en/taxonomy/term/2" title="">Discussion</a></li>
+        <li class="menu-516"><a href="/en/taxonomy/term/5" title="">Bibliographical Explanation</a></li>
+        <li class="menu-764 last"><a href="/en/category/articles">ALL</a></li>
+      </ul>
+      <h2>부 메뉴</h2>
+      <ul id="secondary-menu" class="links inline clearfix">
+        <li class="menu-524 first"><a href="/en/archive/person" title="">Overview</a></li>
+        <li class="menu-610"><a href="/en/kyeol" title="">About Kyeol</a></li>
+        <!-- <li class="menu-935 last"><a href="/en/newsletters" title="">뉴스레터</a></li> -->
+      </ul>
+    </nav>
     <form class="cf01" method="post" action="/en/search">
       <a href="#" class="btn_icon01" title="Close search box"><i class="xi-close"></i></a>
       <fieldset>

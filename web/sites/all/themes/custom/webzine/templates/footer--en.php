255da6d86e9ddf321a889db02a9f3753baf4724f
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-01-29 23:22:09 +0900

    [국문] 웹진 결 사이트 우측 하단에 아카이브 814 로고 및 링크 연결 #34

--- web/sites/all/themes/custom/webzine/templates/footer--en.php
+++ web/sites/all/themes/custom/webzine/templates/footer--en.php
@@ -1,11 +1,14 @@
-<div class="addrA">
-  <address>(04505) 4th Floor, Central Place, 50 Seosomun-ro, Jung-Gu, Seoul.</address>
-  <p class="cpr">Copyright 2020 Webzine by the Research Institute of Japanese Military Sexual Slavery (RIMSS) <br/>All Rights Reserved.</p>
+<div class="clearfix">
+  <div class="addrA">
+    <address>(04505) 4th Floor, Central Place, 50 Seosomun-ro, Jung-Gu, Seoul.</address>
+    <p class="cpr">Copyright 2020 Webzine by the Research Institute of Japanese Military Sexual Slavery (RIMSS) <br/>All Rights Reserved.</p>
+  </div>
+  <ul class="infoA">
+    <li class="l1"><strong>Publish :</strong><span>RIMSS</span></li>
+    <li class="l2"><strong>Planning :</strong><a href="">Somethingspecial.studio</a></li>
+    <li class="l3"><strong>Design&Dev :</strong><a href="http://durumi.io/">Team Durumi</a></li>
+    <li class="l4"><strong>Edit :</strong><span>Sora Kim</span></li>
+    <li class="l5"><strong>Contact :</strong><a href="mailto:remember814@stop.or.kr ">remember814@stop.or.kr</a></li>
+  </ul>
 </div>
-<ul class="infoA">
-  <li class="l1"><strong>Publish :</strong><span>RIMSS</span></li>
-  <li class="l2"><strong>Planning :</strong><a href="">Somethingspecial.studio</a></li>
-  <li class="l3"><strong>Design&Dev :</strong><a href="http://durumi.io/">Team Durumi</a></li>
-  <li class="l4"><strong>Edit :</strong><span>Sora Kim</span></li>
-  <li class="l5"><strong>Contact :</strong><a href="mailto:remember814@stop.or.kr ">remember814@stop.or.kr</a></li>
-</ul>
+<!-- <a style="float:right; margin-top: 1.5rem;" target="_blank" href="https://www.archive814.or.kr/"><img src="/sites/all/themes/custom/webzine/images/814-logo.png" alt="아카이브814"></a> -->

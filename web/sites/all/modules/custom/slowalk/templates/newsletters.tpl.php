2c732fad1e345ac93fd1f0f53b709914d8f5ac6f
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-08-03 16:47:19 +0900

    뉴스레터 템플릿 마크업 오류를 바로잡습니다.

--- web/sites/all/modules/custom/slowalk/templates/newsletters.tpl.php
+++ web/sites/all/modules/custom/slowalk/templates/newsletters.tpl.php
@@ -6,7 +6,6 @@
       <?php foreach($data['newsletters'] as $year => $newsletters): ?>
       <dl class="unit unit02">
         <dt><?php echo $year; ?></dt>
-        <br/>
         <dd class="my-3 border-bottom py-3">
           <ul class="newsletters year-2020">
           <?php foreach($newsletters as $newsletter): ?>
@@ -25,24 +24,3 @@
     <!-- //con01 -->
   </div>
 </div>
-
-<style>
-.newsletters {list-style: none; padding: 0; margin: 0}
-.newsletters li {list-style: none; padding: 0; margin: 0 0 1.5rem 0}
-.newsletters li > a {
-  font-size: 20px;
-  line-height: 32px;
-  color: #111;
-  font-weight: 600;
-  max-height: 64px;
-  display: block;
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-.newsletters li > .date {
-color: #888;
-}
-</style>

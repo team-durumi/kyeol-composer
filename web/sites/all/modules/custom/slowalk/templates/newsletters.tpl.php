7e8c41a2b6870ec4ecd54094fda5ec86a44499cc
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-08-03 16:42:01 +0900

    뉴스레터 템플릿 마크업 오류를 바로잡습니다.

--- web/sites/all/modules/custom/slowalk/templates/newsletters.tpl.php
+++ web/sites/all/modules/custom/slowalk/templates/newsletters.tpl.php
@@ -1,32 +1,29 @@
-<div class="fc03" id="main-content">
-    <div class="inner">
-        <div class="cBody">
-          <!-- con01 -->
-          <div class="con01">
-            <?php foreach($data['newsletters'] as $year => $newsletters): ?>
-            <dl class="unit unit02">
-              <dt><?php echo $year; ?></dt>
-              <br/>
-              <dd class="my-3 border-bottom py-3">
-                <ul class="newsletters year-2020">
-                <?php foreach($newsletters as $newsletter): ?>
-                  <li>
-                    <span class="date"><?php print $newsletter['date']; ?></span>
-                    <a target="_blank" href="/sites/default/files/newsletters/<?php print $newsletter['directory'] ?>/">
-                      <?php echo htmlspecialchars($newsletter['title']); ?>
-                    </a>
-                  </li>
-                <?php endforeach; ?>
-                </ul>
-              </dd>
-            </dl>
-            <?php endforeach; ?>
-        </div>
-          <!-- //con01 -->
-        </div>
-      </section>
 
+<div class="inner">
+  <div class="cBody">
+    <!-- con01 -->
+    <div class="con01">
+      <?php foreach($data['newsletters'] as $year => $newsletters): ?>
+      <dl class="unit unit02">
+        <dt><?php echo $year; ?></dt>
+        <br/>
+        <dd class="my-3 border-bottom py-3">
+          <ul class="newsletters year-2020">
+          <?php foreach($newsletters as $newsletter): ?>
+            <li>
+              <span class="date"><?php print $newsletter['date']; ?></span>
+              <a target="_blank" href="/sites/default/files/newsletters/<?php print $newsletter['directory'] ?>/">
+                <?php echo htmlspecialchars($newsletter['title']); ?>
+              </a>
+            </li>
+          <?php endforeach; ?>
+          </ul>
+        </dd>
+      </dl>
+      <?php endforeach; ?>
     </div>
+    <!-- //con01 -->
+  </div>
 </div>
 
 <style>
@@ -46,6 +43,6 @@
   text-overflow: ellipsis;
 }
 .newsletters li > .date {
-  color: #888;
+color: #888;
 }
 </style>

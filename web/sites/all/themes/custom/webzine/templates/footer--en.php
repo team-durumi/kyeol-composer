e68038e2a20d7a2df8d0269d30f919d3a48d5a38
woonjjang <woonjjang@gmail.com>
2021-08-12 09:05:34 +0900

    #48 푸터 편집책임자 영역을 삭제합니다.

--- web/sites/all/themes/custom/webzine/templates/footer--en.php
+++ web/sites/all/themes/custom/webzine/templates/footer--en.php
@@ -7,8 +7,8 @@
     <li class="l1"><strong>Publish :</strong><span>RIMSS</span></li>
     <li class="l2"><strong>Planning :</strong><a href="">Somethingspecial.studio</a></li>
     <li class="l3"><strong>Design&Dev :</strong><a href="//durumi.io/">Team Durumi</a></li>
-    <li class="l4"><strong>Edit :</strong><span>Sora Kim</span></li>
-    <li class="l5"><strong>Contact :</strong><a href="mailto:remember814@stop.or.kr ">remember814@stop.or.kr</a></li>
+    <li class="l4"><strong>Contact :</strong><a href="mailto:remember814@stop.or.kr">remember814@stop.or.kr</a></li>
+    <!-- li class="l5"><strong>Contact :</strong><a href="mailto:remember814@stop.or.kr ">remember814@stop.or.kr</a></li-->
   </ul>
 </div>
 <!-- <a style="float:right; margin-top: 1.5rem;" target="_blank" href="https://www.archive814.or.kr/"><img src="/sites/all/themes/custom/webzine/images/814-logo.png" alt="아카이브814"></a> -->

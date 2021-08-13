ab412a2d90be2753d3dba5727d17a52ec41b4a5c
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-08-13 12:18:44 +0900

    [웹접근 심사] 웹진 결 국영문 #47 -- 푸터 블록 템플릿에 로고 이미지의 텍스트와 동일한 대체 텍스트를 추가합니다.

---
+++ web/sites/all/modules/custom/slowalk/modules/block/templates/block--wz-block--wz-footer.tpl.php
@@ -0,0 +1,19 @@
+<div id="block-wz-block-wz-footer" class="block block-wz-block">
+  <div class="clearfix">
+    <div class="addrA">
+      <address>서울특별시 중구 서소문로 50 센트럴플레이스 4층 한국여성인권진흥원 일본군'위안부'문제연구소&nbsp;(04505)</address>
+      <p class="cpr">Copyright 2021&nbsp;한국여성인권진흥원 일본군'위안부'문제연구소. All Rights Reserved.</p>
+    </div>
+
+    <ul class="infoA"><li class="l1"><strong>발행인&nbsp;: 박봉정숙</strong></li>
+      <li class="l2"><strong>편집인 : 일본군'위안부'문제연구소</strong></li>
+      <li class="l3"><strong>기획 : 일본군'위안부'문제연구소</strong></li>
+      <li class="l4"><strong>제작 : 퍼플레이컴퍼니</strong></li>
+      <li class="l5"><strong>문의 :</strong><a href="mailto:remember814@stop.or.kr ">remember814@stop.or.kr</a></li>
+    </ul>
+  </div>
+
+  <a href="https://www.archive814.or.kr/" style="float:right; margin-top: 1.5rem;" target="_blank">
+    <img alt="814 일본군'위안부'문제연구소 아카이브814" src="/sites/all/themes/custom/webzine/images/814-logo.png" style="width: 260px;">
+  </a>
+</div>

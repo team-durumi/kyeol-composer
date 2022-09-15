9f202072b46bf236852ba383017822b80b598099
Woonjang Han <woonjjang@gmail.com>
2022-09-15 02:11:53 +0000

    헤더 디자인을 바꾸고 웹접근 작업을 처리해 봅니다. #66

--- web/sites/all/themes/custom/webzine/templates/header.php
+++ web/sites/all/themes/custom/webzine/templates/header.php
@@ -12,32 +12,31 @@ $action = '/search' . ((current_path() == 'search/term') ? '/term' : '');
   <div class="inner">
     <h1 class="lg01">
       <a href="<?php print $front_page;?>">결|일본군'위안부'문제연구소 웹진</a>
-      <em><b><?php print $vol;?></b><i>.vol</i></em>
     </h1>
-      <ul class="global-nav ml-10 mt-10 float-left text-white">
-        <li class="mx-6 float-left">
-          <a href="/featured" title="특집">특집</a>
+      <ul class="global-nav ml-10 mt-8 float-left text-gray-900 rounded-full bg-white">
+        <li class="mx-6 float-left py-2">
+          <a href="/featured" title="특집 메뉴">특집</a>
         </li>
-        <li class="mx-6 float-left">
-          <a href="/category/인터뷰" title="인터뷰">인터뷰</a>
+        <li class="mx-6 float-left py-2">
+          <a href="/category/인터뷰" title="인터뷰 메뉴">인터뷰</a>
         </li>
-        <li class="mx-6 float-left">
-          <a href="/category/에세이" title="에세이">에세이</a>
+        <li class="mx-6 float-left py-2">
+          <a href="/category/에세이" title="에세이 메뉴">에세이</a>
         </li>
-        <li class="mx-6 float-left">
-          <a href="/category/논평" title="논평">논평</a>
+        <li class="mx-6 float-left py-2">
+          <a href="/category/논평" title="논평 메뉴">논평</a>
         </li>
-        <li class="mx-6 float-left">
-          <a href="/category/좌담" title="좌담">좌담</a>
+        <li class="mx-6 float-left py-2">
+          <a href="/category/좌담" title="좌담 메뉴">좌담</a>
         </li>
-        <li class="mx-6 float-left">
-          <a href="/category/자료해제" title="자료해제">자료해제</a>
+        <li class="mx-6 float-left py-2">
+          <a href="/category/자료해제" title="자료해제 메뉴">자료해제</a>
         </li>
       </ul>
       <ul class="ng01">
-        <li class="search"><a href="#" title="검색창 열기"><i class="xi-search"></i></a></li>
-        <li class="menu"><a href="#" title="메뉴 열기"><i class="xi-bars"></i></a></li>
-        <li class="lg:text-3xl sm:text- text-white"><a href="/en" title="switch to EN" >EN</a></li>
+        <li class="search focus:outline-none focus:ring"><a href="#" title="검색창 열기"><i class="xi-search"></i></a></li>
+        <li class="menu focus:outline-none focus:ring"><a href="#" title="메뉴 열기"><i class="xi-bars"></i></a></li>
+        <li class="swichtoen lg:text-3xl sm:text- text-white element-focusable focus:outline-none focus:ring"><a href="/en" title="영문 웹진으로 이동" >EN</a></li>
       </ul>
       <nav class="displayNone">
           <a href="#" class="btn_icon01" title="메뉴 닫기"><i class="xi-close"></i></a>

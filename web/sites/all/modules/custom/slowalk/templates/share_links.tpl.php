8be0e853f078db0c1bbef35898916b6ee88e5feb
Woonjang Han <woonjjang@gmail.com>
2022-10-17 05:37:32 +0000

    share button에 포커스링이 잡히도록 처리합니다.

--- web/sites/all/modules/custom/slowalk/templates/share_links.tpl.php
+++ web/sites/all/modules/custom/slowalk/templates/share_links.tpl.php
@@ -1,19 +1,19 @@
-<a class="resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Facebook">
+<a class="focus:outline-2 focus:ring focus:border-blue-900 resp-sharing-button__link" href="https://facebook.com/sharer/sharer.php?u=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Facebook">
   <div class="resp-sharing-button resp-sharing-button--facebook resp-sharing-button--large">
-    <div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <div class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm3.6 11.5h-2.1v7h-3v-7h-2v-2h2V8.34c0-1.1.35-2.82 2.65-2.82h2.35v2.3h-1.4c-.25 0-.6.13-.6.66V9.5h2.34l-.24 2z"/></svg>
     </div>
   </div>
 </a>
-<a class="resp-sharing-button__link" href="https://twitter.com/intent/tweet/?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Twitter">
+<a class="focus:outline-2 focus:ring focus:border-blue-900 resp-sharing-button__link" href="https://twitter.com/intent/tweet/?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Twitter">
   <div class="resp-sharing-button resp-sharing-button--twitter resp-sharing-button--large">
-    <div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <div class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm5.26 9.38v.34c0 3.48-2.64 7.5-7.48 7.5-1.48 0-2.87-.44-4.03-1.2 1.37.17 2.77-.2 3.9-1.08-1.16-.02-2.13-.78-2.46-1.83.38.1.8.07 1.17-.03-1.2-.24-2.1-1.3-2.1-2.58v-.05c.35.2.75.32 1.18.33-.7-.47-1.17-1.28-1.17-2.2 0-.47.13-.92.36-1.3C7.94 8.85 9.88 9.9 12.06 10c-.04-.2-.06-.4-.06-.6 0-1.46 1.18-2.63 2.63-2.63.76 0 1.44.3 1.92.82.6-.12 1.95-.27 1.95-.27-.35.53-.72 1.66-1.24 2.04z"/></svg>
     </div>
   </div>
 </a>
 <!--
-<a class="resp-sharing-button__link" href="https://currents.google.com/share?url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on google current">
+<a class="focus:outline-2 focus:ring focus:border-blue-900 resp-sharing-button__link" href="https://currents.google.com/share?url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on google current">
   <div class="resp-sharing-button resp-sharing-button--google resp-sharing-button--large">
     <div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
       <svg viewBox="0 0 960 960" xmlns="http://www.w3.org/2000/svg"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm167 633.6C638.4 735 583 757 516.9 757c-95.7 0-178.5-54.9-218.8-134.9C281.5 589 272 551.6 272 512s9.5-77 26.1-110.1c40.3-80.1 123.1-135 218.8-135 66 0 121.4 24.3 163.9 63.8L610.6 401c-25.4-24.3-57.7-36.6-93.6-36.6-63.8 0-117.8 43.1-137.1 101-4.9 14.7-7.7 30.4-7.7 46.6s2.8 31.9 7.7 46.6c19.3 57.9 73.3 101 137 101 33 0 61-8.7 82.9-23.4 26-17.4 43.2-43.3 48.9-74H516.9v-94.8h230.7c2.9 16.1 4.4 32.8 4.4 50.1 0 74.7-26.7 137.4-73 180.1z"/></svg>
@@ -21,9 +21,9 @@
   </div>
 </a>
 -->
-<a class="resp-sharing-button__link" href="https://telegram.me/share/url?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Telegram">
+<a class="focus:outline-2 focus:ring focus:border-blue-900 resp-sharing-button__link" href="https://telegram.me/share/url?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Telegram">
   <div class="resp-sharing-button resp-sharing-button--telegram resp-sharing-button--large">
-    <div aria-hidden="true" class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
+    <div class="resp-sharing-button__icon resp-sharing-button__icon--solidcircle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 23.5c6.35 0 11.5-5.15 11.5-11.5S18.35.5 12 .5.5 5.65.5 12 5.65 23.5 12 23.5zM2.505 11.053c-.31.118-.505.738-.505.738s.203.62.513.737l3.636 1.355 1.417 4.557a.787.787 0 0 0 1.25.375l2.115-1.72a.29.29 0 0 1 .353-.01L15.1 19.85a.786.786 0 0 0 .746.095.786.786 0 0 0 .487-.573l2.793-13.426a.787.787 0 0 0-1.054-.893l-15.568 6z" fill-rule="evenodd"/></svg>
     </div>
   </div>

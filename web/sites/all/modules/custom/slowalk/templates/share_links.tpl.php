b3959680848093c21f30db46ea946413eed7b71c
woonjjang <woonjjang@gmail.com>
2023-08-21 23:25:16 +0900

    페이스북 div 오류를 수정합니다.

--- web/sites/all/modules/custom/slowalk/templates/share_links.tpl.php
+++ web/sites/all/modules/custom/slowalk/templates/share_links.tpl.php
@@ -1,8 +1,6 @@
 <button class="sharing-button">
   <a href="https://facebook.com/sharer/sharer.php?u=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Facebook">
-    <div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm3.6 11.5h-2.1v7h-3v-7h-2v-2h2V8.34c0-1.1.35-2.82 2.65-2.82h2.35v2.3h-1.4c-.25 0-.6.13-.6.66V9.5h2.34l-.24 2z"/></svg>
-    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm3.6 11.5h-2.1v7h-3v-7h-2v-2h2V8.34c0-1.1.35-2.82 2.65-2.82h2.35v2.3h-1.4c-.25 0-.6.13-.6.66V9.5h2.34l-.24 2z"/></svg>
   </a>  
 </button>
 <button class="sharing-button">

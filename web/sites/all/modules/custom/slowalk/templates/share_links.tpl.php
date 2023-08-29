f55d31f8925c41b88bd747ce3447fcb6ee4597be
woonjjang <woonjjang@gmail.com>
2023-08-29 13:10:05 +0900

    버튼과 a tag 이중포커스를 없애고, 링크 텍스트를 제공합니다.

--- web/sites/all/modules/custom/slowalk/templates/share_links.tpl.php
+++ web/sites/all/modules/custom/slowalk/templates/share_links.tpl.php
@@ -1,14 +1,9 @@
-<a href="https://facebook.com/sharer/sharer.php?u=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Facebook">
+<a href="https://facebook.com/sharer/sharer.php?u=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" title="페이스북으로 글을 공유합니다.">
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm3.6 11.5h-2.1v7h-3v-7h-2v-2h2V8.34c0-1.1.35-2.82 2.65-2.82h2.35v2.3h-1.4c-.25 0-.6.13-.6.66V9.5h2.34l-.24 2z"/></svg>
-</a> 
-<button class="sharing-button">
-  <a href="https://twitter.com/intent/tweet/?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Twitter">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm5.26 9.38v.34c0 3.48-2.64 7.5-7.48 7.5-1.48 0-2.87-.44-4.03-1.2 1.37.17 2.77-.2 3.9-1.08-1.16-.02-2.13-.78-2.46-1.83.38.1.8.07 1.17-.03-1.2-.24-2.1-1.3-2.1-2.58v-.05c.35.2.75.32 1.18.33-.7-.47-1.17-1.28-1.17-2.2 0-.47.13-.92.36-1.3C7.94 8.85 9.88 9.9 12.06 10c-.04-.2-.06-.4-.06-.6 0-1.46 1.18-2.63 2.63-2.63.76 0 1.44.3 1.92.82.6-.12 1.95-.27 1.95-.27-.35.53-.72 1.66-1.24 2.04z"/></svg>
-  </a>
-</button>
-<button class="sharing-button">
-  <a href="https://telegram.me/share/url?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" aria-label="Share on Telegram">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 23.5c6.35 0 11.5-5.15 11.5-11.5S18.35.5 12 .5.5 5.65.5 12 5.65 23.5 12 23.5zM2.505 11.053c-.31.118-.505.738-.505.738s.203.62.513.737l3.636 1.355 1.417 4.557a.787.787 0 0 0 1.25.375l2.115-1.72a.29.29 0 0 1 .353-.01L15.1 19.85a.786.786 0 0 0 .746.095.786.786 0 0 0 .487-.573l2.793-13.426a.787.787 0 0 0-1.054-.893l-15.568 6z" fill-rule="evenodd"/></svg>
-  </a>
-</button>
-
+</a>
+<a href="https://twitter.com/intent/tweet/?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" title="트위터로 글을 공유합니다.">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 0C5.38 0 0 5.38 0 12s5.38 12 12 12 12-5.38 12-12S18.62 0 12 0zm5.26 9.38v.34c0 3.48-2.64 7.5-7.48 7.5-1.48 0-2.87-.44-4.03-1.2 1.37.17 2.77-.2 3.9-1.08-1.16-.02-2.13-.78-2.46-1.83.38.1.8.07 1.17-.03-1.2-.24-2.1-1.3-2.1-2.58v-.05c.35.2.75.32 1.18.33-.7-.47-1.17-1.28-1.17-2.2 0-.47.13-.92.36-1.3C7.94 8.85 9.88 9.9 12.06 10c-.04-.2-.06-.4-.06-.6 0-1.46 1.18-2.63 2.63-2.63.76 0 1.44.3 1.92.82.6-.12 1.95-.27 1.95-.27-.35.53-.72 1.66-1.24 2.04z"/></svg>
+</a>
+<a href="https://telegram.me/share/url?text=<?php print $data['text']; ?>&amp;url=<?php print $data['canonical_url']; ?>" target="_blank" rel="noopener" title="텔레그램으로 글을 공유합니다.">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 23.5c6.35 0 11.5-5.15 11.5-11.5S18.35.5 12 .5.5 5.65.5 12 5.65 23.5 12 23.5zM2.505 11.053c-.31.118-.505.738-.505.738s.203.62.513.737l3.636 1.355 1.417 4.557a.787.787 0 0 0 1.25.375l2.115-1.72a.29.29 0 0 1 .353-.01L15.1 19.85a.786.786 0 0 0 .746.095.786.786 0 0 0 .487-.573l2.793-13.426a.787.787 0 0 0-1.054-.893l-15.568 6z" fill-rule="evenodd"/></svg>
+</a>

2921209909004a0d84fbf580c142719b81181954
woonjjang <woonjjang@gmail.com>
2023-08-30 10:12:15 +0900

    about-us 건너뛰기 적용을 합니다.

--- web/sites/all/themes/custom/webzine/template.php
+++ web/sites/all/themes/custom/webzine/template.php
@@ -248,7 +248,7 @@ function webzine_breadcrumb($variables) {
     // Provide a navigational heading to give context for breadcrumb links to
     // screen-reader users. Make the heading invisible with .element-invisible.
     $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
-    $output .= '<div class="breadcrumb">' . implode(' ', $breadcrumb) . '</div>';
+    $output .= '<div class="breadcrumb" id="main-content">' . implode(' ', $breadcrumb) . '</div>';
     return $output;
   }
 }

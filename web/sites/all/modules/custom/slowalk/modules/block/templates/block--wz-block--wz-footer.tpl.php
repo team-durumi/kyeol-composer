2f8d1af1182a1a0746b23c6e5582976b26a4c8c8
woonjjang <woonjjang@gmail.com>
2023-11-10 15:07:14 +0900

    switch_wa_mark_2023

--- web/sites/all/modules/custom/slowalk/modules/block/templates/block--wz-block--wz-footer.tpl.php
+++ web/sites/all/modules/custom/slowalk/modules/block/templates/block--wz-block--wz-footer.tpl.php
@@ -2,7 +2,7 @@
   <div class="clearfix">
     <div class="addrA">
       <address>서울특별시 중구 서소문로 50 센트럴플레이스 4층 한국여성인권진흥원 일본군'위안부'문제연구소&nbsp;(04505)</address>
-      <p class="cpr">Copyright 2021&nbsp;한국여성인권진흥원 일본군'위안부'문제연구소. All Rights Reserved.</p>
+      <p class="cpr">Copyright 2023&nbsp;한국여성인권진흥원 일본군'위안부'문제연구소. All Rights Reserved.</p>
     </div>
 
     <ul class="infoA"><li class="l1"><strong>발행인&nbsp;: 일본군'위안부'문제연구소</strong></li>
@@ -13,7 +13,7 @@
     </ul>
   </div>
   <a title="새창" href="http://www.wa.or.kr/board/list.asp?BoardID=0006" target="_blank" class="wa-mark">
-    <img class="wa" alt="(사)한국장애인단체총연합회 한국웹접근성인증평가원 웹 접근성 우수사이트 인증마크(WA인증마크_2022갱신)" src="/sites/all/themes/custom/webzine/images/wa_mark_220906.png" />
+    <img class="wa" alt="(사)한국장애인단체총연합회 한국웹접근성인증평가원 웹 접근성 우수사이트 인증마크(WA인증마크_2023갱신)" src="/sites/all/themes/custom/webzine/images/wa_mark_230906.png" />
   </a>􀀁
   <a href="https://www.archive814.or.kr/" style="float:right; margin-top: 1.5rem;" target="_blank">
     <img alt="814 일본군'위안부'문제연구소 아카이브814" src="/sites/all/themes/custom/webzine/images/814-logo.png" style="width: 260px;">

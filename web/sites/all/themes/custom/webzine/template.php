eac9dabe14b828fe0ae39b2a00e7ab484a8120b7
woonjjang <woonjjang@gmail.com>
2024-05-02 23:48:50 +0900

    jsdelivr.net 오류 확인하고 복원합니다.

--- web/sites/all/themes/custom/webzine/template.php
+++ web/sites/all/themes/custom/webzine/template.php
@@ -15,7 +15,7 @@ define('__WZ__', drupal_get_path('theme', 'webzine'));
 function webzine_js_alter(&$javascript)
 {
   $javascript['misc/jquery.js']['version'] = '3.5.0';
-  $javascript['misc/jquery.js']['data'] = '//fastly.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.min.js';
+  $javascript['misc/jquery.js']['data'] = '//cdn.jsdelivr.net/npm/jquery@3.5.0/dist/jquery.min.js';
 }
 
 
@@ -105,14 +105,14 @@ function webzine_preprocess_page(&$variables) {
   }
   drupal_add_js(array('Webzine' => array('vol' => '/vol/' . $vol)), 'setting');
   drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js', array('type' => 'external', 'scope' => 'header', 'group' => JS_LIBRARY));
-  drupal_add_js('//fastly.jsdelivr.net/npm/jquery-validation@1.19.0/dist/jquery.validate.min.js', array('type' => 'external', 'scope' => 'header', 'group' => JS_LIBRARY ));
+  drupal_add_js('//cdn.jsdelivr.net/npm/jquery-validation@1.19.0/dist/jquery.validate.min.js', array('type' => 'external', 'scope' => 'header', 'group' => JS_LIBRARY ));
   drupal_add_js('//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js', array('type' => 'external', 'scope' => 'header', 'group' => JS_THEME ));
   drupal_add_js(drupal_get_path('module', 'ckeditor') . '/plugins/cavacnote/jquery/jquery.cavacnote.js', array('type' => 'file', 'scope' => 'header', 'group' => JS_LIBRARY));
   drupal_add_js(drupal_get_path('module', 'slowalk'). '/js/cavacnotify.js', array('type' => 'file', 'scope' => 'footer', 'group' => JS_THEME));
   drupal_add_js(drupal_get_path('theme', 'webzine') . '/js/jquery.dialogOptions.js', array('type' => 'file', 'scope' => 'header', 'group' => JS_THEME));
   drupal_add_css('//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css', array('type' => 'external', 'group' => CSS_THEME));
   drupal_add_css('//cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.css', array('type' => 'external', 'group' => CSS_THEME));
-  drupal_add_css('//fastly.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css', array('type' => 'external', 'group' => CSS_THEME));
+  drupal_add_css('//cdn.jsdelivr.net/npm/xeicon@2.3.3/xeicon.min.css', array('type' => 'external', 'group' => CSS_THEME));
   drupal_add_css('//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css', array('type' => 'external', 'group' => CSS_THEME));
   drupal_add_css(drupal_get_path('module', 'ckeditor') . '/plugins/cavacnote/css/cavacnote.css', array('type' => 'file', 'group' => CSS_THEME));
   drupal_add_css('//unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css', array('type' => 'external', 'group' => CSS_THEME));

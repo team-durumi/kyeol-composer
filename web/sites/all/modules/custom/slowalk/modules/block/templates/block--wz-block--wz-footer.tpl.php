1a9ae6efd0790cfeb1fe6d61b7b7f790c39d401c
woonjjang <woonjjang@gmail.com>
2024-05-02 14:59:35 +0900

    제작사 수정

--- web/sites/all/modules/custom/slowalk/modules/block/templates/block--wz-block--wz-footer.tpl.php
+++ web/sites/all/modules/custom/slowalk/modules/block/templates/block--wz-block--wz-footer.tpl.php
@@ -8,7 +8,7 @@
     <ul class="infoA"><li class="l1"><strong>발행인&nbsp;: 일본군'위안부'문제연구소</strong></li>
       <li class="l2"><strong>편집인 : 일본군'위안부'문제연구소</strong></li>
       <li class="l3"><strong>기획 : 일본군'위안부'문제연구소</strong></li>
-      <li class="l4"><strong>제작 : 퍼플레이컴퍼니</strong></li>
+      <li class="l4"><strong>제작 : 스컹크웍스스튜디오</strong></li>
       <li class="l5"><strong>문의 :</strong><a href="mailto:remember814@stop.or.kr ">remember814@stop.or.kr</a></li>
     </ul>
   </div>
